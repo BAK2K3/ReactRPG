a1dd64b67b09f934baa5f96ce8a0e73d9e79a6ed
benjamin-a-kavanagh <benjamin.a.kavanagh@github.pwc.com>
2021-11-18 09:12:22 +0000

    Add serializer for Codex.

---
+++ reactrpg/backend/codex/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Codex
+
+
+class CodexSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Codex
+        fields = ['name', 'alpha_name', 'type', 'base_hp', 'base_attack', 'base_defence',
+                  'base_speed', 'image', 'paid', 'min_level']
