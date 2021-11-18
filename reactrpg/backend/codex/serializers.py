704da67004f956583bce1f4ce4e56d4419d7c577
benjamin-a-kavanagh <benjamin.a.kavanagh@github.pwc.com>
2021-11-18 10:45:29 +0000

    Update typo in serializer.

--- reactrpg/backend/codex/serializers.py
+++ reactrpg/backend/codex/serializers.py
@@ -5,5 +5,5 @@ from .models import Codex
 class CodexSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Codex
-        fields = ['name', 'alpha_name', 'type', 'base_hp', 'base_attack', 'base_defence',
+        fields = ['name', 'alpha_name', 'type', 'base_hp', 'base_attack', 'base_defense',
                   'base_speed', 'image', 'paid', 'min_level']
