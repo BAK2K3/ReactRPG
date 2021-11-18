f904a2fc29e7b0652064f523c0e87983a8d25e6e
benjamin-a-kavanagh <benjamin.a.kavanagh@github.pwc.com>
2021-11-18 10:46:55 +0000

    Update route for codex urls.

--- reactrpg/urls.py
+++ reactrpg/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
 from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('codex/', include('backend.codex.urls'))
+    path('codex/', include('reactrpg.backend.codex.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
