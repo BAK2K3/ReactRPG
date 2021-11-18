68d870791a62ab1843121b8041136e35106d9635
benjamin-a-kavanagh <benjamin.a.kavanagh@github.pwc.com>
2021-11-18 09:12:50 +0000

    Add Codex API Viewset.

--- reactrpg/backend/codex/views.py
+++ reactrpg/backend/codex/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from .models import Codex
+from rest_framework import permissions, viewsets
+from .serializers import CodexSerializer
 
-# Create your views here.
+
+class CodexViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    API endpoint that allows Codex to be viewed.
+    """
+    queryset = Codex.objects.get_random()
+    serializer_class = CodexSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
